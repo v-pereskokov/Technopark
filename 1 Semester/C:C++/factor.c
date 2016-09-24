62f033b0162aae1855e1aee3427441d7698bc3f7
Vladislav Pereskokov <vladpereskokov@mail.ru>
2016-09-24 13:31:10 +0300

    Update factor.c

--- 1 Semester/C:C++/factor.c
+++ 1 Semester/C:C++/factor.c
@@ -2,95 +2,93 @@
 #include <stdlib.h>
 #include <limits.h>
 
-typedef enum { false, true } bool;
 typedef unsigned long u_long;
 
 const int MAX_SIZE = 65;
-const int TRIVIAL = 600000;
+const int TRIVIAL = 10000000;
 
 u_long *myCalloc(size_t size)
 {
-    u_long *array = (u_long *) malloc(size * sizeof(u_long));
-    if (!array) {
-        printf("[error]\n");
-        return NULL;
-    }
-    for (size_t i = 0; i < size; ++i)
-        array[i] = 1;
-    return array;
+  u_long *array = (u_long *) malloc(size * sizeof(u_long));
+  if (!array) {
+    printf("[error]\n");
+    return NULL;
+  }
+  for (size_t i = 0; i < size; ++i)
+    array[i] = 1;
+  return array;
 }
 
 void myFree(u_long *array)
 {
-    if (array)
-        free(array);
+  if (array)
+    free(array);
 }
 
 void transport(u_long *lhs, const u_long *rhs)
 {
-    size_t i = 0;
-    while (rhs[i] != 0) {
-        lhs[i] = rhs[i];
-        ++i;
-    }
-    lhs[i] = 0;
+  size_t i = 0;
+  while (rhs[i] != 0) {
+    lhs[i] = rhs[i];
+    ++i;
+  }
+  lhs[i] = 0;
 }
 
 u_long *trivialFactor(u_long target)
 {
-    u_long *result = NULL;
-    if (target == 1) {
-        result = myCalloc(2);
-        result[1] = 0;
-        return NULL;
-    } else {
-        u_long *tmp = myCalloc(MAX_SIZE);
-        size_t i = 1;
-        for (size_t j = 2; j * j <= target; ++j) {
-            while (target % j == 0) {
-                tmp[i] = j;
-                target /= j;
-                ++i;
-            }
-        }
-        if (target > 1)
-            tmp[i] = target;
-        if (tmp[i] != 1)
-            ++i;
-        tmp[i] = 0;
-        result = myCalloc(i + 1);
-        transport(result, tmp);
-        myFree(tmp);
+  u_long *result = NULL;
+  if (target == 1) {
+    result = myCalloc(2);
+    result[1] = 0;
+    return NULL;
+  } else {
+    u_long *tmp = myCalloc(MAX_SIZE);
+    size_t i = 1;
+    for (size_t j = 2; j * j <= target; ++j) {
+      while (target % j == 0) {
+        tmp[i] = j;
+        target /= j;
+        ++i;
+      }
     }
-    return result;
+    if (target > 1)
+      tmp[i] = target;
+    if (tmp[i] != 1)
+      ++i;
+    tmp[i] = 0;
+    result = myCalloc(i + 1);
+    transport(result, tmp);
+    myFree(tmp);
+  }
+  return result;
 }
 
 u_long *factor(u_long target)
 {
-    u_long *result = NULL;
-    result = trivialFactor(target);
-    return result;
+  u_long *result = NULL;
+  result = trivialFactor(target);
+  return result;
 }
 
 void print(u_long *array)
 {
-    size_t i = 0;
-    while (array[i] != 0)
-        printf("%ld ", array[i++]);
-    printf("\n");
+  size_t i = 0;
+  while (array[i] != 0)
+    printf("%ld ", array[i++]);
+  printf("\n");
 }
 
 int main()
 {
-    u_long target;
-    scanf("%ld", &target);
-    if (target <= LONG_MAX) {
-        u_long *result = factor(target);
-        if (result != NULL) {
-            print(result);
-            myFree(result);
-        }
-    } else
-        printf("[error]\n");
-    return 0;
+  u_long target;
+  if (scanf("%ld", &target) == 1 && target > 0 && target <= TRIVIAL) {
+    u_long *result = factor(target);
+    if (result != NULL) {
+      print(result);
+      myFree(result);
+    }
+  } else
+    printf("[error]\n");
+  return 0;
 }
