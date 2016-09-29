9446b5286a46c43aad30e0aa0fbf49baeb4a12dc
Vladislav Pereskokov <vladpereskokov@mail.ru>
2016-09-29 13:47:58 +0300

    Add files via upload

--- Semester_1/Alghorhitm/3_3.cpp
+++ Semester_1/Alghorhitm/3_3.cpp
@@ -7,6 +7,8 @@ using u_int = unsigned int;
 
 template <class IT>
 IT BinarySearch(IT begin, IT end, int element) {
+  if (begin == end)
+    return end;
   IT first = begin;
   IT last = end;
   --last;
@@ -20,38 +22,43 @@ IT BinarySearch(IT begin, IT end, int element) {
   return (first == end || *first != element ) ? end : first;
 }
 
-bool search(std::vector<int> *A, std::vector<int> *B) {
-  auto begin_A = A->begin();
-  auto end_A = A->end();
-  auto index = begin_A;
-  for (auto it = B->begin(); it < B->end(); ++it) {
-    index = BinarySearch(index, end_A, (*it));
-    if (index == end_A)
-      return false;
+void search(std::vector<int> *a, std::vector<int> *b, std::vector<int> *c) {
+  auto begin_a = a->begin();
+  auto end_a = a->end();
+  auto index = begin_a;
+  auto pre_index = begin_a;
+  for (auto it = b->begin(); it < b->end(); ++it) {
+    index = BinarySearch(index, end_a, *it);
+    if (index == end_a) {
+      index = pre_index;
+      continue;
+    }
+    pre_index = index;
+    c->push_back(*it);
+    index += 1;
   }
-  return true;
 }
 
-int main() {
-  u_int n, m;
-  scanf("%d %d", &n, &m);
-  if (n > 0 && m > 0 && n <= 10000 && m <= 10000 && n > m) {
-    int element;
-    std::vector<int> A;
-    for (std::size_t i = 1; i <= n; ++i) {
-      scanf("%d", &element);
-      A.push_back(element);
-    }
-    std::vector<int> B;
-    for (std::size_t i = 1; i <= m; ++i) {
-      scanf("%d", &element);
-      B.push_back(element);
-    }
-    if (search(&A, &B)) {
-      for (auto i : B)
+  int main() {
+    u_int n, m;
+    scanf("%d %d", &n, &m);
+    if (n > 0 && m > 0 && n <= 10000 && m <= 10000 && n > m) {
+      int element;
+      std::vector<int> a;
+      for (std::size_t i = 1; i <= n; ++i) {
+        scanf("%d", &element);
+        a.push_back(element);
+      }
+      std::vector<int> b;
+      for (std::size_t i = 1; i <= m; ++i) {
+        scanf("%d", &element);
+        b.push_back(element);
+      }
+      std::vector<int> c;
+      search(&a, &b, &c);
+      for (auto i : c)
         printf("%d ", i);
       printf("\n");
+      return 0;
     }
   }
-  return 0;
-}
---
+++ Semester_1/Alghorhitm/5_1.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+bool isBracket(const char bracket) {
+  switch (bracket) {
+    case '{':
+    case '}':
+    case '[':
+    case ']':
+    case '(':
+    case ')':
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool isClosed(const char lhs, const char rhs) {
+  std::string cmp = "";
+  cmp += lhs; cmp += rhs;
+  return cmp == "()" || cmp == "[]" || cmp == "{}";
+}
+
+bool isOpen(const char bracket) {
+  return bracket == '(' || bracket == '[' || bracket == '{';
+}
+
+bool otherSide_(const char lhs, const char rhs) {
+  
+  return !isOpen(lhs) && isOpen(rhs);
+}
+
+bool oneSide(const char lhs, const char rhs) {
+  return isOpen(lhs) && isOpen(rhs);
+}
+
+char inverseBracket(const char bracket) {
+  switch (bracket) {
+    case '(':
+      return ')';
+    case ')':
+      return '(';
+    case '[':
+      return ']';
+    case ']':
+      return '[';
+    case '{':
+      return '}';
+    case '}':
+      return '{';
+    default:
+      return ' ';
+  }
+}
+
+std::string bracket(std::string *target) {
+  std::stack<char> brackets;
+  for (std::size_t i = 0; i < (*target).size(); ++i) {
+    char bracket = (*target)[i];
+    if (isBracket(bracket)) {
+      if (brackets.size() > 0) {
+        char topBracket = brackets.top();
+        if (isClosed(topBracket, bracket)) {
+          brackets.pop();
+        } else if (oneSide(topBracket, bracket) || otherSide_(topBracket, bracket)) {
+          brackets.push(bracket);
+        } else {
+          return "IMPOSSIBLE";
+        }
+      } else {
+        brackets.push(bracket);
+      }
+    } else {
+      return "IMPOSSIBLE";
+    }
+  }
+  std::string before = "";
+  while (brackets.size() > 0) {
+    char bracket = brackets.top();
+    if (isOpen(bracket)) {
+      *target += inverseBracket(bracket);
+    } else {
+      before += inverseBracket(bracket);
+    }
+    brackets.pop();
+  }
+  before += *target;
+  return before;
+}
+
+int main() {
+  std::string target;
+  std::cin >> target;
+  std::string result = bracket(&target);
+  std::cout << result << std::endl;
+  return 0;
+}
