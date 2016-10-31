b37490dc21d697e558db665bfe5ee0f820ca46eb
vladpereskokov <vladpereskokov@mail.ru>
2016-10-31 20:56:53 +0300

    4

--- Semester_1/Algorhitm/m2/4_4.cpp
+++ Semester_1/Algorhitm/m2/4_4.cpp
@@ -6,24 +6,10 @@
 
 using data_type = int;
 using size_type = size_t;
+using statistic_type = size_t;
 
-bool less(const data_type &lhs, const data_type &rhs) {
+bool less(const data_type lhs, const data_type rhs) {
   return lhs < rhs;
-}
-
-template <class IT, class comparator>
-IT partition_(IT begin, IT end, comparator &comp) {
-  IT pivot = end;
-  IT it = begin - 1;
-  for (IT jt = begin; jt != pivot; ++jt) {
-    if (!less(*pivot, *jt)) {
-      ++it;
-      std::swap(*it, *jt);
-    }
-  }
-  ++it;
-  std::swap(*it, *pivot);
-  return it;
 }
 
 size_t random(const size_type begin, const size_type end) {
@@ -33,10 +19,58 @@ size_t random(const size_type begin, const size_type end) {
 }
 
 template <class IT, class comparator>
+IT partition_(IT begin, IT end, comparator &comp) {
+  IT pivot = end;
+  IT it = begin - 1;
+  for (IT jt = begin; jt != pivot; ++jt) {
+    if (!comp(*pivot, *jt)) {
+      ++it;
+      std::swap(*it, *jt);
+    }
+  }
+  ++it;
+  std::swap(*it, *pivot);
+  return it;
+}
+
+template <class IT, class comparator>
+IT partition1_(IT begin, IT end, comparator &comp) {
+  IT pivot = begin;
+  IT it = end - 1;
+  for (IT jt = end - 1; jt != pivot; --jt) {
+    if (comp(*pivot, *jt)) {
+      std::swap(*it, *jt);
+      --it;
+    }
+  }
+  std::swap(*it, *pivot);
+  return it;
+}
+
+template <class IT, class comparator>
 IT randomPartition(IT begin, IT end, comparator &comp) {
   size_type pivot = random(0, std::distance(begin, end) - 1);
-  std::swap(*begin, *(begin + pivot));
+  if (pivot != 0) {
+    std::swap(*begin, *(begin + pivot));
+  }
   return partition_(begin, end, comp);
+}
+
+template <class IT, class comparator>
+data_type findStatistic(IT begin, IT end, statistic_type k, comparator &comp) {
+  IT first = begin;
+  size_type pivot = -1;
+  while (pivot != k) {
+    pivot = std::distance(randomPartition(begin, end, less), end - 1);
+    std::cout << pivot << std::endl;
+    if (k < pivot) {
+      end = first + pivot;
+    } else if (k > pivot) {
+      k -= pivot + 1;
+      begin = first + pivot + 1;
+    }
+  }
+  return *(first + pivot);
 }
 
 template <class IT, class comparator>
@@ -49,7 +83,7 @@ void quickSort(IT begin, IT end, comparator &comp) {
 }
 
 int main() {
-  std::vector<data_type> v{-8, 2, 3, 5, 1, 0, 1, 5, 6};
+  std::vector<data_type> v{21, -2, 0};
   quickSort(v.begin(), v.end(), less);
   for (auto i : v) {
     std::cout << i << " ";
