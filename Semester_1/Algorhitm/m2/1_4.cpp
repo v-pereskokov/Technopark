283fec5b1eaadd32500eae58e5f18e045e7cefa3
vladpereskokov <vladpereskokov@mail.ru>
2016-11-07 17:27:32 +0300

    1_4

--- Semester_1/Algorhitm/m2/1_4.cpp
+++ Semester_1/Algorhitm/m2/1_4.cpp
@@ -1,16 +1,34 @@
 #include <cstdio>
 #include <vector>
 #include <string>
-#include <Python.h>
+#include <algorithm>
 
+using Symbol = char;
+using cWord = char;
 using Word = std::string;
 using Words = std::vector<Word>;
 
-template <class IT>
-void insertionSort(IT begin, IT end) {
+bool less(const Symbol lhs, const Symbol rhs) {
+  return lhs < rhs;
+}
+
+template <class IT, class comparator>
+bool cmpString(IT begin1, IT end1, IT begin2, IT end2, const comparator &comp) {
+  for (IT it = begin1, jt = begin2; it != end1 && jt != end2; ++it, ++jt) {
+    if (comp(*jt, *it)) {
+      return false;
+    } else if (comp(*it, *jt)) {
+      return true;
+    }
+  }
+  return std::distance(begin1, end1) > std::distance(begin2, end2) ? false : true;
+}
+
+template <class IT, class comparator>
+void insertionSort(IT begin, IT end, const comparator &comp) {
   for (auto it = begin + 1; it != end; ++it) {
     for (auto jt = it - 1; jt != begin - 1; --jt) {
-      if (*jt > *(jt + 1)) {
+      if (!cmpString(jt->begin(), jt->end(), (jt + 1)->begin(), (jt + 1)->end(), less)) {
         std::swap(*jt, *(jt + 1));
       }
     }
@@ -22,11 +40,11 @@ int main() {
   scanf("%ld", &n);
   Words words;
   for (std::size_t i = 1; i <= n; ++i) {
-    char word[255];
+    cWord word[255];
     scanf("%s", word);
     words.push_back(word);
   }
-  insertionSort(words.begin(), words.end());
+  insertionSort(words.begin(), words.end(), less);
   for (auto i : words) {
     printf("%s\n", i.c_str());
   }
