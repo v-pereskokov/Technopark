1eb265c11c0ba3cbed1da7aa4cd5d48fe4d1e6d6
vladpereskokov <vladpereskokov@mail.ru>
2016-12-06 09:56:46 +0300

    asd: m2 - 7, m3 - 1, 2; web: hw5

---
+++ Semester_1/Web/HW5/ask/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+  url(r'^$', views.index, name='index'),
+  url(r'^$(?P<page>\d+)/', views.index, name='index'),
+
+  url(r'^hot/$', views.questions, name='hot_questions'),
+  url(r'^hot/?page=(?P<page>\d+)?/$', views.questions, name='hot questions'),
+
+  url(r'^tag/(?P<tag>\w+)/?$', views.questions_tag, name='questions tag'),
+  url(r'^tag/(?P<tag>\w+)/(?P<page>\d+)/$', views.questions_tag, name='questions tag'),
+
+  url(r'^question/id(?P<id>\d+)/?$', views.question, name='question'), 
+  url(r'^ask/', views.ask_page, name='ask page'),
+  url(r'^login/', views.login, name='login'),
+  url(r'^signup/', views.signup, name='signup'),
+  url(r'^user/(?P<user_name>\w+)/?$', views.user, name='user'),
+]
